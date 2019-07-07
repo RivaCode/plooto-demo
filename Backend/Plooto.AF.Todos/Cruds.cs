c4e1c2abf29fdbb89cfa90d9a5370c60f7730c20
Stas Rivkin <slavariv@gmail.com>
2019-07-07 17:41:43 -0400

    mapping api

--- Backend/Plooto.AF.Todos/Cruds.cs
+++ Backend/Plooto.AF.Todos/Cruds.cs
@@ -1,33 +1,61 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Plooto.AF.Todos
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName(nameof(GetTodos))]
+        public static async Task<IActionResult> GetTodos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todos")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("all todos");
+        }
 
-            string name = req.Query["name"];
+        [FunctionName(nameof(GetTodosSuggestion))]
+        public static async Task<IActionResult> GetTodosSuggestion(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todos/suggestion")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("all todos");
+        }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        [FunctionName(nameof(GetTodoById))]
+        public static async Task<IActionResult> GetTodoById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todos/{id}")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(nameof(GetTodoById));
+        }
+
+        [FunctionName(nameof(PostTodo))]
+        public static async Task<IActionResult> PostTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todos")] HttpRequest req,
+            ILogger log)
+        {
+            return new CreatedResult("", nameof(PostTodo));
+        }
+
+        [FunctionName(nameof(PutTodoById))]
+        public static async Task<IActionResult> PutTodoById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todos/{id}")] HttpRequest req,
+            ILogger log)
+        {
+            return new NoContentResult();
+        }
+
+        [FunctionName(nameof(DeleteTodoById))]
+        public static async Task<IActionResult> DeleteTodoById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todos/{id}")] HttpRequest req,
+            ILogger log)
+        {
+            return new NoContentResult();
         }
     }
 }
