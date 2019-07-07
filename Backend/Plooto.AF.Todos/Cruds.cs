0e5098567df46132d4a4e22ae0c2019329182df9
Stas Rivkin <slavariv@gmail.com>
2019-07-07 17:48:00 -0400

    adding user operations

--- Backend/Plooto.AF.Todos/Cruds.cs
+++ Backend/Plooto.AF.Todos/Cruds.cs
@@ -50,12 +50,28 @@ namespace Plooto.AF.Todos
             return new NoContentResult();
         }
 
+        [FunctionName(nameof(PutTodoByIdAssign))]
+        public static async Task<IActionResult> PutTodoByIdAssign(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todos/{id}/assign")] HttpRequest req,
+        ILogger log)
+        {
+            return new NoContentResult();
+        }
+
         [FunctionName(nameof(DeleteTodoById))]
         public static async Task<IActionResult> DeleteTodoById(
             [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todos/{id}")] HttpRequest req,
             ILogger log)
         {
             return new NoContentResult();
+        }
+
+        [FunctionName(nameof(GetUsers))]
+        public static async Task<IActionResult> GetUsers(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")] HttpRequest req,
+        ILogger log)
+        {
+            return new OkObjectResult("all users");
         }
     }
 }
